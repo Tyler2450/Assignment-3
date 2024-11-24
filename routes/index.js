115ad34c0a1d67c3929258216ccf58e6e3b70cd7
Tyler2450 <tfurtado0311@gmail.com>
2024-11-23 19:17:29 -0500

    Part 3

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
-// routes/index.js
 const express = require('express');
 const router = express.Router();
 const Workout = require('../models/workout');
 
-// Route for homepage
+// Home route
 router.get('/', (req, res) => {
-  res.render('index');  // This should correspond to the index.ejs file in your views folder
+  res.render('index', { title: 'Home' }); // Your home page
 });
 
-// Route for viewing workouts
+// Workouts route
 router.get('/workouts', async (req, res) => {
   try {
-    const workouts = await Workout.find({});
-    res.render('workouts', { workouts: workouts });  // This should correspond to workouts.ejs in your views folder
+    const workouts = await Workout.find(); // Fetch all workouts
+    console.log(workouts);  // Check that all data is being fetched
+    res.render('workouts', { workouts }); // Pass workouts data to the view
   } catch (err) {
     res.status(500).send({
       message: 'Something went wrong!',
-      error: err.message
+      error: err.message,
     });
   }
 });
